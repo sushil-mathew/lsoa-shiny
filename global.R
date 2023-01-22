d7a393c9a0552d9bf06bae10d69aa180bc109292
Sushil Mathew <73226314+sushil-mathew@users.noreply.github.com>
2023-01-22 00:09:01 +0000

    added more mapdeck stuff

--- global.R
+++ global.R
@@ -1,15 +1,7 @@
-library(tidyverse)
-library(conflicted)
-library(sf)
-library(sp)  # deal with spatial data
+load("data/mydata.Rdata")
 
+library(mapdeck)
+# TODO how would this work on github or shiny server?
+# set API token for mapbox 
+set_token("pk.eyJ1IjoibWF0aGV3c3VzaGlsIiwiYSI6ImNsZDZrYWhjcDBncm0zcXBseWFoZHEyankifQ.gn3neKhfwQDbqntx70d8Ng")
 
-conflict_prefer("select", "dplyr")
-shp <- st_read("data/lsoa_dz_merged/lsoa_dz_merged.shp") %>%
-  st_transform(crs = 4326)
-
-shp$norm <- runif(nrow(shp))
-
-
-mydata <- shp %>%
-  dplyr::select(-TotPop2011, -ResPop2011, -HHCnt2011, -StdAreaHa, -Shape_Area, -BNG_E, -BNG_N)  # select all columns except the coords
